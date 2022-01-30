5d5eba6663067dca9c21587effb9af14dc661390
Dennis Dunn <ansofive@gmail.com>
2022-01-30 10:53:08 -0700

    doesn't work.

--- src/grammar.js
+++ src/grammar.js
@@ -1,59 +1,39 @@
-const { regex, trim, literal, between, number, any, sequence } = require('../lib');
+const { regex, trim, literal, optional, between, number, any, sequence } = require('../lib');
 
-/**
-<exp> ::= <exp> + <term> | <exp> - <term> | <term>
-<term> ::= <term> * <power> | <term> / <power> | <power>
-<power> ::= <factor> ^ <power> | <factor>
-<factor> ::= ( <exp> ) | <number>
-*/
 
-/**
-expOp = + | -
-termOp = * | /
-powerOp = ^
-lParen = (
-rParen = )
 
-exp = exp expOp term | term
-term = term termOp power | power
-power = factor powerOp power | factor
-factor = lParen exp rParen | number
-*/
-
-/**
-exp = term expPr
-expPr = expOp term expPr
-term = power termPr
-termPr = termOp power termPr
-power = factor powerOp power | factor
-factor = lParen exp rParen | num
-*/
-
-const expOp = trim(regex(/[-+]/));
-const termOp = trim(regex(/[*\/]/));
-const powerOp = trim(regex(/\^/));
-const lParen = trim(literal('('));
-const rParen = trim(literal(')'));
-
-let exp;
-let power;
-let termPr;
-let expPr;
-
+const sum = trim(regex(/[-+]/));
+const pro = trim(regex(/[*\/]/));
+const pow = trim(regex(/\^/));
+const lparen = trim(literal('('));
+const rparen = trim(literal(')'));
 const num = trim(number);
-const factor = any(between(lParen, exp, rParen), num);
-power = any(sequence(factor, powerOp, power), factor);
-termPr = sequence(termOp, power, termPr);
-const term = sequence(power, termPr);
-expPr = sequence(expOp, term, expPr);
-exp = sequence(term, expPr);
+
+let E;
+let Ep;
+let T;
+let Tp;
+let P;
+let F;
+
+E = sequence(T, Ep);
+Ep = optional(sequence(sum, T, Ep));
+T = sequence(P, Tp);
+Tp = optional(sequence(pro, P, Tp));
+P = any(sequence(F, pow, P), F);
+F = any(sequence(lparen, E, rparen), num);
 
 module.exports = {
-    num,
-    factor,
-    power,
-    termPr,
-    term,
-    expPr,
-    exp
+    E,
+    Ep,
+    T,
+    Tp,
+    P,
+    F,
+    sum,
+    pro,
+    pow,
+    lparen,
+    rparen,
+    num
 }
--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -12,7 +12,7 @@ test('parse a number with whitespace', () => {
 })
 
 test('parse a power', () => {
-    const r = run(G.power, "3^2");
+    const r = run(G.P, "3^2");
     console.log(r)
     expect(r.result).toStrictEqual([3, '^', 2]);
 })
