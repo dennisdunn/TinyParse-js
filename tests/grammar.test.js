923c55fa0f9afd108889d80680f54d2be35bf12c
Dennis Dunn <ansofive@gmail.com>
2022-02-02 15:13:34 -0700

    Switched the grammar tests to use the parse() function.

--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -1,29 +1,26 @@
-const G = require('../src');
-const { context } = require('../lib');
-
-const run = (parser, text) => parser(context(text));
+const { parse } = require('../src');
 
 test('parse 1+1', () => {
-    const r = run(G.E, '1+1');
+    const r = parse('1+1');
     expect(r.result).toStrictEqual([1, '+', 1])
 })
 
 test('parse 2*2', () => {
-    const r = run(G.E, '2*2');
+    const r = parse('2*2');
     expect(r.result).toStrictEqual([2, '*', 2])
 })
 
 test('parse 2*2+1', () => {
-    const r = run(G.E, '2*2+1');
+    const r = parse('2*2+1');
     expect(r.result).toStrictEqual([2, '*', 2, '+', 1])
 })
 
 test('parse 2*(1+1)', () => {
-    const r = run(G.E, '2*(1+1)');
+    const r = parse('2*(1+1)');
     expect(r.result).toStrictEqual([2, '*', '(', 1, '+', 1, ')'])
 })
 
 test('parse 2*(1+1)', () => {
-    const r = G.parse('2*(1+1)');
+    const r = parse('2*(1+1)');
     expect(r.result).toStrictEqual([2, '*', '(', 1, '+', 1, ')'])
 })
