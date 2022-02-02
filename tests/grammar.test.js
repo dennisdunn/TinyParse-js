f2212fa86141acfe2b90fbe27833f35f55fd0a03
Dennis Dunn <ansofive@gmail.com>
2022-02-02 15:34:27 -0700

    The library "sequence" now returns a tree instead of a flat list. Fixed the tests to accomodate.

--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -1,26 +1,28 @@
 const { parse } = require('../src');
 
+const flatten = arr => arr.flat(Infinity);
+
 test('parse 1+1', () => {
     const r = parse('1+1');
-    expect(r.result).toStrictEqual([1, '+', 1])
+    expect(flatten(r.result)).toStrictEqual([1, '+', 1])
 })
 
 test('parse 2*2', () => {
     const r = parse('2*2');
-    expect(r.result).toStrictEqual([2, '*', 2])
+    expect(flatten(r.result)).toStrictEqual([2, '*', 2])
 })
 
 test('parse 2*2+1', () => {
     const r = parse('2*2+1');
-    expect(r.result).toStrictEqual([2, '*', 2, '+', 1])
+    expect(flatten(r.result)).toStrictEqual([2, '*', 2, '+', 1])
 })
 
 test('parse 2*(1+1)', () => {
     const r = parse('2*(1+1)');
-    expect(r.result).toStrictEqual([2, '*', '(', 1, '+', 1, ')'])
+    expect(flatten(r.result)).toStrictEqual([2, '*', '(', 1, '+', 1, ')'])
 })
 
 test('parse 2*(1+1)', () => {
     const r = parse('2*(1+1)');
-    expect(r.result).toStrictEqual([2, '*', '(', 1, '+', 1, ')'])
+    expect(flatten(r.result)).toStrictEqual([2, '*', '(', 1, '+', 1, ')'])
 })
