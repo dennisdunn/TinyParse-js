e27198d7565672521e8a9aa723b80113ff0673f2
Dennis Dunn <ansofive@gmail.com>
2022-02-02 15:11:17 -0700

    Removed crufty tests.

--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -3,21 +3,6 @@ const { context } = require('../lib');
 
 const run = (parser, text) => parser(context(text));
 
-test('parse a number', () => {
-    const r = run(G.number, '123');
-    expect(r.result).toBe(123);
-})
-
-test('parse F (number)', () => {
-    const r = run(G.F, '123');
-    expect(r.result).toBe(123);
-})
-
-test('parse F (parenthesized E)', () => {
-    const r = run(G.F, '(123)');
-    expect(r.result).toStrictEqual(['(', 123, ')'])
-})
-
 test('parse 1+1', () => {
     const r = run(G.E, '1+1');
     expect(r.result).toStrictEqual([1, '+', 1])
