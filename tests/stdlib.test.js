0ba3c64f3f78f9c35d38eca21c450e715bd9a0c6
Dennis Dunn <ansofive@gmail.com>
2022-02-11 13:13:15 -0700

    Added tests for the stdlib parsers. Bumped patch number.

---
+++ tests/stdlib.test.js
@@ -0,0 +1,22 @@
+const { flat, join, symbols, number, alphanum, hexDigit, whitespace, digit,many, Stream } = require('../lib');
+
+test('parse a number', () => {
+    const p = number;
+    const r = p(new Stream('123'));
+
+    expect(r).toBe('123');
+})
+
+test('parse a hex number', () => {
+    const p = join(many(hexDigit));
+    const r = p(new Stream('af123b'));
+
+    expect(r).toBe('af123b');
+})
+
+test('parse a word', () => {
+    const p = join(many(alphanum));
+    const r = p(new Stream('AF123b'));
+
+    expect(r).toBe('AF123b');
+})
