b33d9cc6b16d5cc481af9a0f97d1f442e69f1df3
Dennis Dunn <ansofive@gmail.com>
2022-02-18 19:50:57 -0700

    Oops. Forgot to parse a negative sign! Bumped the patch number.

--- tests/stdlib.test.js
+++ tests/stdlib.test.js
@@ -7,6 +7,24 @@ test('parse a number', () => {
     expect(r).toBe('123');
 })
 
+test('parse a number (negative sign)', () => {
+    const r = number(new Stream('-123'));
+
+    expect(r).toBe('-123');
+})
+test('parse a decimal', () => {
+    const p = number;
+    const r = p(new Stream('123.1'));
+
+    expect(r).toBe('123.1');
+})
+
+test('parse a decimal (negative sign)', () => {
+    const r = number(new Stream('-123.1'));
+
+    expect(r).toBe('-123.1');
+})
+
 test('parse a hex number', () => {
     const p = join(many(hexDigit));
     const r = p(new Stream('af123b'));
